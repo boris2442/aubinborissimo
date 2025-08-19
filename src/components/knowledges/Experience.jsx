06a84c43d194d38b0a569dc79dfe5d9c6b20744f
boris2442 <aubinborissimotsebo@gmail.com>
2025-08-19 13:19:59 +0200

    add campuspay same experience

--- src/components/knowledges/Experience.jsx
+++ src/components/knowledges/Experience.jsx
@@ -16,6 +16,17 @@ const Experience = () => {
 
                     Basée sur des standards élevés de qualité et de durabilité, Life adopte des procédés modernes de traitement d’eau pour garantir la pureté, le goût et la sécurité sanitaire de ses produits. Elle ambitionne de devenir une référence dans le domaine de l’eau embouteillée en Afrique, tout en intégrant des pratiques respectueuses de l’environnement et en favorisant le développement local.</p>
             </div>
+              <div className="exp-3">
+                <h4 style={{color:'#10182A'}}>Développeur Full-Stack && Chef de projet CAMPUSPAY</h4>
+                <h5>2024 - 2025</h5>
+                <p>CampusPay est une plateforme numérique conçue pour simplifier et sécuriser la gestion des paiements académiques dans les établissements scolaires et universitaires.
+                    Elle permet aux étudiants, parents et institutions de réaliser et de suivre les transactions financières de manière rapide, transparente et fiable.
+
+                    En tant que développeur Full-Stack et chef de projet, j’ai participé à la conception, au développement et au déploiement de la solution.
+                    J’ai notamment travaillé sur l’architecture technique, l’intégration des moyens de paiement (Orange Money, MTN Mobile Money, PayPal), la gestion des utilisateurs et la mise en place d’un tableau de bord interactif pour l’administration.
+
+                    Ce projet illustre ma capacité à allier compétences techniques et vision produit pour répondre à des problématiques concrètes de digitalisation et de transformation numérique dans le secteur éducatif.</p>
+            </div>
         </div>
     );
 };
